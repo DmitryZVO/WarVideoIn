a4ec9b39fd5a8bffc609c7a6b137e120ef14d23f
Дмитрий Звонарев <zvonarev@dealer-it.ru>
2025-10-31 21:50:20 +0300

    Добавьте файлы проекта.

---
+++ WarVideoIn/FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WarVideoIn
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBoxMain = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMain).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxMain
+            // 
+            pictureBoxMain.BackColor = Color.Black;
+            pictureBoxMain.Location = new Point(12, 12);
+            pictureBoxMain.Name = "pictureBoxMain";
+            pictureBoxMain.Size = new Size(800, 600);
+            pictureBoxMain.TabIndex = 0;
+            pictureBoxMain.TabStop = false;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(824, 801);
+            Controls.Add(pictureBoxMain);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FormMain";
+            ShowIcon = false;
+            Text = "Radio Video IN";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMain).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxMain;
+    }
+}
